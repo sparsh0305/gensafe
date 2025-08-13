203b6f7830604af0cb97a4c564057a5e75f461ce
sparsh0305 <sparsh.aggarwal05@gmail.com>
2025-08-13 17:26:10 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,5 @@
-
-// Simple cache-first service worker
 const CACHE_NAME = 'gensafe-static-v1';
-const PRECACHE_URLS = ['/', '/index.html', '/manifest.json', '/styles.css'];
+const PRECACHE_URLS = ['/', '/index.html', '/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(PRECACHE_URLS)));
@@ -13,14 +11,16 @@ self.addEventListener('activate', event => {
 });
 
 self.addEventListener('fetch', event => {
+  // Only respond to GET
+  if (event.request.method !== 'GET') return;
   event.respondWith(
-    caches.match(event.request).then(resp => 
-      resp || fetch(event.request).then(resp2 => {
+    caches.match(event.request).then(cacheResp => {
+      return cacheResp || fetch(event.request).then(networkResp => {
         return caches.open('gensafe-dynamic-v1').then(cache => {
-          cache.put(event.request, resp2.clone());
-          return resp2;
+          cache.put(event.request, networkResp.clone());
+          return networkResp;
         });
-      })
-    ).catch(()=>caches.match('/offline.html'))
+      });
+    }).catch(()=> caches.match('/index.html'))
   );
 });
